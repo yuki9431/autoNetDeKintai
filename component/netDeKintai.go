591277f343fe75efe558a2e996d5afadcd45ae34
yuki9431 <yuki9431@gmail.com>
2019-08-05 15:41:40 +0900

    mod:change filename

---
+++ component/netDeKintai.go
@@ -0,0 +1,123 @@
+package component
+
+import (
+	"fmt"
+
+	"github.com/sclevine/agouti"
+)
+
+type User struct {
+	Id       string
+	Password string
+}
+
+type WebInfo struct {
+	User User
+	Page *agouti.Page
+}
+
+// come to work: true
+// leave to work: false
+func Punch(user User, isCome bool) (err error) {
+	web, err := new(user)
+	if err != nil {
+		return
+	}
+
+	if err := web.accessToNetdeKomon(); err != nil {
+		return fmt.Errorf("Error: Faild to access netDeKomon: %v", err)
+	}
+
+	if err := web.login(); err != nil {
+		return fmt.Errorf("Error: Faild to login %v", err)
+	}
+
+	if err := web.punch(isCome); err != nil {
+		return fmt.Errorf("Error: Faild to punch %v", err)
+	}
+
+	if err := web.logout(); err != nil {
+		return fmt.Errorf("Error: Faild to logout %v", err)
+	}
+
+	return nil
+}
+
+func new(user User) (web *WebInfo, err error) {
+	// start driver
+	driver := agouti.ChromeDriver()
+	if err = driver.Start(); err != nil {
+		return nil, fmt.Errorf("Error: Failed to start driver:%v", err.Error())
+	}
+	// defer driver.Stop()
+
+	page, err := driver.NewPage(agouti.Browser("chrome"))
+	if err != nil {
+		return nil, fmt.Errorf("Error: Failed to open page:%v", err.Error())
+	}
+
+	return &WebInfo{
+		User: User{
+			Id:       user.Id,
+			Password: user.Password,
+		},
+		Page: page,
+	}, nil
+}
+
+func (web *WebInfo) accessToNetdeKomon() (err error) {
+	if err := web.Page.Navigate("https://www1.shalom-house.jp/komon/"); err != nil {
+		return fmt.Errorf("Error: Failed to navigate:%v", err.Error())
+	}
+
+	err = web.Page.ClearCookies()
+	if err != nil {
+		return
+	}
+
+	return nil
+}
+
+func (web *WebInfo) login() (err error) {
+
+	err = web.Page.AllByID("txtID").Fill(web.User.Id)
+	if err != nil {
+		return
+	}
+
+	err = web.Page.AllByID("txtPsw").Fill(web.User.Password)
+	if err != nil {
+		return
+	}
+
+	err = web.Page.AllByID("btnLogin").Click()
+	if err != nil {
+		return
+	}
+
+	err = web.Page.AllByID("ctl00_ContentPlaceHolder1_imgBtnSyuugyou").Click()
+	if err != nil {
+		return
+	}
+
+	return nil
+}
+
+func (web *WebInfo) logout() (err error) {
+	err = web.Page.AllByID("ctl00_LnkbtnLogOut").Click()
+	if err != nil {
+		return
+	}
+
+	return nil
+}
+
+func (web *WebInfo) punch(isCome bool) (err error) {
+	if isCome == true {
+		// come to work
+		return web.Page.AllByID("ctl00_ContentPlaceHolder1_ibtnIn3").Click()
+	} else {
+		// leave to work
+		return web.Page.AllByID("ctl00_ContentPlaceHolder1_imgOut3").Click()
+	}
+}
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"autoNetDeKintai/component"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var usage = `Usage:
+  netDeCommon [options] -usr [<userId>] -pwd [<password>]
+  netDeCommon -come -usr UserId -pwd Password
+
+Options:
+  -h                  ヘルプを表示.
+  -come               出社に打刻.
+  -leave              退社に打刻.
+  -usr <userId>       ユーザIDを入力できる.
+  -pwd <password>     パスワードを入力できる.`
+
+func main() {
+	// help
+	help := flag.Bool("h", false, "ヘルプを表示")
+
+	// get userId and password from option
+	userId := flag.String("usr", "", "enter your userId")
+	password := flag.String("pwd", "", "enter your password")
+
+	// get punch type
+	//isCome := flag.Bool("come", false, "use when come to work")
+	//isLeave := flag.Bool("leave", false, "use when leave to work")
+	flag.Parse()
+
+	if *help == true {
+		fmt.Println(usage)
+		os.Exit(0)
+	}
+
+	if *userId == "" || *password == "" {
+		log.Fatalf("Error: enter userId and password")
+	}
+
+	userInfo := component.User{
+		Id:       *userId,
+		Password: *password,
+	}
+
+	component.Punch(userInfo, true)
+
+}
